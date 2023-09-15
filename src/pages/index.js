675a86869268be3c06c81491a0478a3f38fd10d8
syfrezia <syafrie.mufariza98@gmail.com>
2023-09-16 06:25:10 +0700

    fix: renamed productPage.jsx to ProductPage.jsx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Home from "./Home";
-import ProductPage from "./productPage";
+import ProductPage from "./ProductPage";
 import CategoryPage from "./CategoryPage";
 import ResultPage from "./ResultPage";
 import CheckoutPage from "./CheckoutPage";
