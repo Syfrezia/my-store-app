97639cd57a3031b1332c8348816d6b513ab0958a
syfrezia <syafrie.mufariza98@gmail.com>
2023-09-16 07:41:33 +0700

    Revert "fix: renamed all file paths in index.js"
    This reverts commit 997a7ab482676b98c08104b274502b0205e36704.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Home from "./Home.jsx";
-import ProductPage from "./ProductPage.jsx";
-import CategoryPage from "./CategoryPage.jsx";
-import ResultPage from "./ResultPage.jsx";
-import CheckoutPage from "./CheckoutPage.jsx";
-import PaymentPage from "./PaymentPage.jsx";
+import Home from "./Home";
+import ProductPage from "./ProductPage";
+import CategoryPage from "./CategoryPage";
+import ResultPage from "./ResultPage";
+import CheckoutPage from "./CheckoutPage";
+import PaymentPage from "./PaymentPage";
 
 export {
   Home,
